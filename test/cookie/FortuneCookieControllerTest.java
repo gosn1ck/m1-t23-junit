fce570735bc06b81acd5cbad7a2149538836455e
Николай Верховцев <gosn1ck@yandex.ru>
2023-01-28 21:53:03 +0300

    feat: cookies test

--- test/cookie/FortuneCookieControllerTest.java
+++ test/cookie/FortuneCookieControllerTest.java
@@ -1,4 +1,43 @@
 package cookie;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
 public class FortuneCookieControllerTest {
+
+    private static FortuneCookieController goodFactoryController;
+    private static FortuneCookieController badFactoryController;
+    private static FortuneCookieFactory goodFactory;
+    private static FortuneCookieFactory badFactory;
+    private static FortuneConfig goodConfig;
+    private static FortuneConfig badConfig;
+
+    @BeforeAll
+    public static void beforeEach() {
+        goodConfig = new FortuneConfig(true);
+        goodFactory = new FortuneCookieFactory(goodConfig,
+                Collections.singletonList("positive"),
+                Collections.singletonList("negative"));
+        goodFactoryController = new FortuneCookieController(goodFactory);
+
+        badConfig = new FortuneConfig(false);
+        badFactory = new FortuneCookieFactory(badConfig,
+                Collections.singletonList("positive"),
+                Collections.singletonList("negative"));
+        badFactoryController = new FortuneCookieController(badFactory);
+    }
+
+    @Test
+    public void shouldReturnPositiveFortune(){
+        Assertions.assertEquals("positive", goodFactoryController.tellFortune().getFortuneText());
+    }
+
+    @Test
+    public void shouldReturnNegativeFortune(){
+        Assertions.assertEquals("negative", badFactoryController.tellFortune().getFortuneText());
+    }
+
 }
--- test/cookie/FortuneCookieFactoryTest.java
+++ test/cookie/FortuneCookieFactoryTest.java
@@ -1,4 +1,39 @@
 package cookie;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
 public class FortuneCookieFactoryTest {
+    private static FortuneCookieFactory fabrica;
+
+    @BeforeEach
+    public void beforeEach() {
+        fabrica = new FortuneCookieFactory(new FortuneConfig(true),
+                Collections.singletonList("positive"),
+                Collections.singletonList("negative"));
+    }
+
+    @Test
+    public void shouldIncrementCountByOneAfterOneCookieBaked(){
+        fabrica.bakeFortuneCookie();
+        Assertions.assertEquals(1, fabrica.getCookiesBaked());
+    }
+
+    @Test
+    public void shouldIncrementCountByTwoAfterTwoCookiesBaked (){
+        fabrica.bakeFortuneCookie();
+        fabrica.bakeFortuneCookie();
+        Assertions.assertEquals(2, fabrica.getCookiesBaked());
+    }
+
+    @Test
+    public void shouldSetCounterToZeroAfterResetCookieCreatedCall (){
+        fabrica.bakeFortuneCookie();
+        Assertions.assertEquals(1, fabrica.getCookiesBaked());
+        fabrica.resetCookiesCreated();
+        Assertions.assertEquals(0, fabrica.getCookiesBaked());
+    }
 }
