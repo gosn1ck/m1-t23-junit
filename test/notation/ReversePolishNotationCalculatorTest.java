b80e76d36f1880f89a52a3375d8412f11f3eb191
Николай Верховцев <gosn1ck@yandex.ru>
2023-01-28 21:53:51 +0300

    feat: add calc tests

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,28 @@
 package notation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ReversePolishNotationCalculatorTest {
 
+    private static ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+
     @Test
     public void shouldCalculateAddition() {
-
+        var result = calculator.calculatePolishNotation("2 3 + ");
+        Assertions.assertEquals(5, result);
     }
+
+    @Test
+    public void shouldCalculateSubtraction() {
+        var result = calculator.calculatePolishNotation("6 9 -");
+        Assertions.assertEquals(-3, result);
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        var result = calculator.calculatePolishNotation("10 10 *");
+        Assertions.assertEquals(100, result);
+    }
+
 }
