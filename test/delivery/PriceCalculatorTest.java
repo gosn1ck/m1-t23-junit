1fcedc9518b0f31f17ae21b507bdd8f85eed247b
Николай Верховцев <gosn1ck@yandex.ru>
2023-01-28 21:53:26 +0300

    feat: add price calc test

--- test/delivery/PriceCalculatorTest.java
+++ test/delivery/PriceCalculatorTest.java
@@ -8,6 +8,7 @@ public class PriceCalculatorTest {
 
     private final PriceCalculator priceCalculator = new PriceCalculator();
 
+    // BIKE
     @Test
     public void shouldThrowExceptionWhenBikeAndDistanceIs0Km() {
         IllegalArgumentException ex = Assertions.assertThrows(
@@ -16,6 +17,78 @@ public class PriceCalculatorTest {
         );
 
         Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
+    }
+
+    @Test
+    public void shouldReturn200ForBikeAndDistanceIs20Km() {
+        var price = priceCalculator.calculatePrice(TransportType.BIKE, 20);
+        Assertions.assertEquals(200, price);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenBikeAndDistanceIs21Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.BIKE, 21)
+        );
+
+        Assertions.assertEquals("Bike can not go for more than 20 km", ex.getMessage());
+    }
+
+    // CAR
+    @Test
+    public void shouldThrowExceptionWhenCarAndDistanceIs0Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.CAR, 0)
+        );
+
+        Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
+    }
+
+    @Test
+    public void shouldReturn7000ForCarAndDistanceIs1000Km() {
+        var price = priceCalculator.calculatePrice(TransportType.CAR, 1000);
+        Assertions.assertEquals(7000, price);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCarAndDistanceIs1001Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.CAR, 1001)
+        );
+
+        Assertions.assertEquals("Car can not go for more than 1000 km", ex.getMessage());
+    }
+
+    // TRUCK
+    @Test
+    public void shouldThrowExceptionWhenTruckAndDistanceIs0Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.TRUCK, 0)
+        );
+
+        Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
+    }
+
+    @Test
+    public void shouldReturn5005ForTruckAndDistanceIs1001Km() {
+        var price = priceCalculator.calculatePrice(TransportType.TRUCK, 1001);
+        Assertions.assertEquals(5005, price);
+    }
+
+    // DRONE
+    @Test
+    public void shouldThrowExceptionWhenDrone() {
+        UnsupportedOperationException ex = Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                generateExecutable(TransportType.DRONE, 500)
+        );
+
+        Assertions.assertEquals("transport type '" + TransportType.DRONE + "' is not handled correctly",
+                ex.getMessage());
     }
 
     private Executable generateExecutable(TransportType type, int distance) {
